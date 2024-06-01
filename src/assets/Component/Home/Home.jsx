e86f76911f673226074d33306ff8aed46bcabcee
shagors <shajjadshagor@gmail.com>
2024-06-01 15:04:05 +0600

    login and register page design and check route

--- src/assets/Component/Home/Home.jsx
+++ src/assets/Component/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Kurbani from "../Kurbani/Kurbani";
+
 const Home = () => {
-    return (
-        <div className=" min-h-96">
-            This is Home page
-        </div>
-    );
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Kurbani />
+    </div>
+  );
 };
 
 export default Home;

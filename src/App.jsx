3b5cd1fd2fdf8ded6b0a619cd7b8fa5805661a59
S-M-ZUBAYER <smzubayer18@gmail.com>
2024-06-03 12:17:19 +0600

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import { routes } from './assets/Component/Routes/Routes'
-=======
-import './App.css'
-import Home from './Component/Home/Home'
->>>>>>> f3fe5d367e96c0b7f39fea9ce934f381139d67f7
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <RouterProvider router={routes}>
 
       </RouterProvider>
-=======
-      <Home />
->>>>>>> f3fe5d367e96c0b7f39fea9ce934f381139d67f7
     </>
   )
 }
